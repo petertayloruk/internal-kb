4e04b5363f8755c8236fa8538c1541bdeabbc761
Peter Taylor <peter@p-taylor.co.uk>
2026-04-20 08:28:53 +0000

    Define BedrockKB data source and permissions

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,30 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-system/deploy-any-branch/
- */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+// 1. Add the Bedrock HTTP Data Source
+const dataStack = backend.data.resources.nestedStacks['data7552DF31'];
+backend.data.addHttpDataSource(
+  'BedrockKB',
+  'https://bedrock-runtime.eu-west-1.amazonaws.com', // Change region if not eu-west-1
+  {
+    authorizationConfig: {
+      signingRegion: 'eu-west-1',
+      signingServiceName: 'bedrock',
+    },
+  }
+);
+
+// 2. Grant permission to the Backend to call your specific Knowledge Base
+backend.data.resources.cfnResources.cfnGraphqlApi.addToRolePolicy(
+  new PolicyStatement({
+    actions: ['bedrock:Retrieve'],
+    resources: ['arn:aws:aoss:eu-west-1:471112926741:collection/595iilxjnd14z2wuttyi'],
+  })
+);
